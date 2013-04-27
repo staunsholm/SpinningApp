a7846006fd2524e3b8b28f837c846bdc7a71d89a
Mikkel Staunsholm <crap2@staunsholm.dk>
2013-04-27 15:18:38 +0200

    tiny bit of styling play file from startTime to endTime (endTime is a hack, since player.position doesn't work atm)

--- app/scripts/controllers/Create.js
+++ app/scripts/controllers/Create.js
@@ -3,11 +3,23 @@
 angular.module('SpinningApp')
     .controller('CreateCtrl', function ($scope, Sessions, Spotify)
     {
+        $scope.query = 'James Blake';
+        $scope.tracks = [];
+
         $scope.doSearch = function()
         {
+            $scope.tracks = [];
             Spotify.search($scope.query, function(result)
             {
-                $scope.searchResult = result;
+                if (result.tracks.length === 0) return;
+
+                for (var i = 0, l = result.tracks.length; i < l; i++)
+                {
+                    result.tracks[i].startTime = 0;
+                    result.tracks[i].endTime = result.tracks[i].duration;
+                }
+
+                $scope.tracks = result.tracks;
                 $scope.$digest();
             });
         };
---
+++ app/scripts/services/Spotify.js
@@ -0,0 +1,87 @@
+'use strict';
+
+angular.module('SpinningApp')
+    .factory('Spotify', function ($timeout)
+    {
+        var currentTrack;
+        var searchResult;
+        var playingStartTime;
+
+        // register when track should stop
+        $timeout(handleStopTime, 200);
+        function handleStopTime()
+        {
+            if (currentTrack && Date.now() - playingStartTime >= currentTrack.endTime - currentTrack.startTime)
+            {
+                models.player.stop();
+                currentTrack = null;
+            }
+
+            $timeout(handleStopTime, 200);
+        }
+
+        var models, Location, Search, TopList, buttons, List, Image;
+        require([
+            '$api/models',
+            '$api/location#Location',
+            '$api/search#Search',
+            '$api/toplists#Toplist',
+            '$views/buttons',
+            '$views/list#List',
+            '$views/image#Image'
+        ], function (_models, _Location, _Search, _TopList, _buttons, _List, _Image)
+        {
+            models = _models;
+            Location = _Location;
+            Search = _Search;
+            TopList = _TopList;
+            buttons = _buttons;
+            List = _List;
+            Image = _Image;
+        });
+
+        return {
+            // search for query, calls update() when some or all data is received
+            search: function (query, update)
+            {
+                if (!Search)
+                {
+                    console.log("Search not ready");
+                    return;
+                }
+
+                searchResult = {
+                    tracks: []
+                };
+
+                var search = Search.search(query);
+
+                search.tracks.snapshot().done(function (a)
+                {
+                    a.loadAll('name').each(function (track)
+                    {
+                        searchResult.tracks.push(track);
+                    });
+                    update(searchResult);
+                });
+            },
+
+            playTrack: function (track, startTime)
+            {
+                currentTrack = track;
+                models.player.playTrack(track);
+                models.player.seek(track.startTime);
+                playingStartTime = Date.now();
+            },
+
+            pauseTrack: function ()
+            {
+                models.player.pause();
+            },
+
+            stopTrack: function ()
+            {
+                models.player.stop();
+            }
+        };
+    });
